ab4896fb5e70625a187f6fd74eb52b026967dcc6
vanita5 <mail@vanita5.de>
2014-07-23 02:41:39 +0200

    Initial commit

---
+++ 1337walls_roulette.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+
+import os
+import sys
+import argparse
+import requests
+import tempfile
+from subprocess import call
+from random import choice
+from PIL import Image, ImageFilter
+
+__version__ = '0.1'
+
+__api_url__ = 'http://1337walls.w8l.org/api/?rows=image'
+
+DEBUG = False
+VERBOSE = False
+
+def console_out(format_str, *args, **kwargs):
+    if VERBOSE:
+        print(format_str.format(*args, **kwargs))
+        
+def console_debug(format_str, *args, **kwargs):
+    if DEBUG and VERBOSE:
+        sys.stderr.write(format_str.format(*args, **kwargs))
+        sys.stderr.write(os.linesep)
+
+def console():
+    # http://docs.python.org/library/argparse.html#module-argparse
+    parser = argparse.ArgumentParser(description='No arguments needed')
+    parser.add_argument("--version", action='version', version="%(prog)s {}".format(__version__))
+    parser.add_argument("-v", "--verbose", dest="verbose", action='store_true', help="show output")
+    parser.add_argument("-b", "--blur", dest="blur", action='store_true', help="blur the image")
+    parser.add_argument("--debug", dest="debug", action='store_true', help="turn on more verbose output")
+    
+    args = parser.parse_args()
+    
+    global DEBUG
+    global VERBOSE
+    DEBUG = args.debug
+    VERBOSE = args.verbose
+    blur = args.blur
+    
+    # dependency test
+    
+    
+    console_out("Lurking on http://1337walls.w8l.org/...")
+    r = requests.get(__api_url__)
+    
+    if r.status_code != 200:
+        console_out("Could not connect to the 1337walls API: {}", r.status_code)
+        return
+        
+    json = r.json()
+    image = choice(json)
+    url = image['image']
+    
+    console_out("Downloading image ({})", url)
+    r = requests.get(url, stream=True)
+    
+    if r.status_code != 200:
+        console_out("Could not download the image. Error code: {}", r.status_code)
+        return
+            
+    console_out("Download successful!")
+    
+    dir = tempfile.gettempdir() + '/1337_wp'
+    with open(dir, 'wb') as f:
+        for chunk in r.iter_content():
+            f.write(chunk)
+            
+    console_debug("Image temporarely saved to {}", f.name)
+    
+    if blur:
+        console_out("Bluring image...")
+        dir = tempfile.gettempdir() + "/1337_wp_blurred.png"
+        try:
+            original = Image.open(f.name)
+            blured = original.filter(ImageFilter.BLUR)
+        
+            blured.save(dir)
+        except:
+            console_out("[PIL] Unable to load image")
+            return
+    
+    console_out("Setting background image...")
+    call(["nitrogen", "--set-zoom-fill", dir])
+    
+if __name__ == "__main__":
+    sys.exit(console())
---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# http://docs.python.org/distutils/setupscript.html
+# http://docs.python.org/2/distutils/examples.html
+
+import sys
+from setuptools import setup
+import ast
+
+name = '1337walls_roulette'
+version = '0.1'
+with open('{}.py'.format(name), 'rU') as f:
+    for node in (n for n in ast.parse(f.read()).body if isinstance(n, ast.Assign)):
+        node_name = node.targets[0]
+        if isinstance(node_name, ast.Name) and node_name.id.startswith('__version__'):
+            version = node.value.s
+            break
+                                            
+if not version:
+    raise RuntimeError('Unable to find version number')
+    
+setup(
+    name=name,
+    version=version,
+    description='downloads random image from 1337walls, blurs it and sets it as desktop wallpaper',
+    author='vanita5',
+    author_email='mail@vanita5.de',
+    url='http://github.com/vanita5/{}'.format(name),
+    py_modules=[name],
+    license="GPLv3",
+    install_requires=['Pillow >= 2.5.1', 'requests >= 2.3.0'],
+    classifiers=[ # https://pypi.python.org/pypi?:action=list_classifiers
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Intended Audience :: End Users/Desktop',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: System :: Shells',
+        'Topic :: Utilities',
+        'Programming Language :: Python :: 2.7',
+    ],
+    entry_points = {
+        'console_scripts': ['{} = {}:console'.format(name, name)]
+    }
+)
